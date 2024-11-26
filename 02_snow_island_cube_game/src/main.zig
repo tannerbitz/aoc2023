0ee26add8ffcf66f3b308828083cedbf57c0511c
Tanner Bitz <tannerbitz@gmail.com>
2024-11-26 17:52:24 -0500

    starts day 2

--- 02_snow_island_cube_game/src/main.zig
+++ 02_snow_island_cube_game/src/main.zig
@@ -1,4 +1,37 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const Draw = struct {
+    red: usize,
+    green: usize,
+    blue: usize,
+};
+const Game = struct {
+    const Self = @This();
+
+    id: usize,
+    draws: std.ArrayList(Draw),
+
+    fn init(allocator: Allocator) Self {
+        return .{
+            .id = 0,
+            .draws = std.ArrayList(Draw).init(allocator),
+        };
+    }
+
+    fn deinit(self: Self) void {
+        self.draws.deinit();
+    }
+};
+
+fn parseGame(input: []const u8, allocator: Allocator) !Game {
+    const idx = std.mem.indexOf(u8, input, ":");
+    const game_str: []const u8 = input[0..idx.?];
+    const draws_str: []const u8 = input[idx.? + 1 ..];
+
+    std.debug.print("'{s}' || '{s}'\n", .{ game_str, draws_str });
+    return Game.init(allocator);
+}
 
 pub fn main() !void {
     var file = try std.fs.cwd().openFile("input.txt", .{});
@@ -9,8 +42,12 @@ pub fn main() !void {
 
     var buf: [1024]u8 = undefined;
 
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        std.debug.print("{s}\n", .{line});
+        const game = try parseGame(line, allocator);
+        defer game.deinit();
     }
 }
 
