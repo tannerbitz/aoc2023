107b9534e1fbb7b23cf766e1ba29075e75ed2a63
Tanner Bitz <tannerbitz@gmail.com>
2024-11-27 13:36:15 -0500

    finished day 2 part 1

--- 02_snow_island_cube_game/src/main.zig
+++ 02_snow_island_cube_game/src/main.zig
@@ -24,13 +24,64 @@ const Game = struct {
     }
 };
 
+fn trimWhitespace(input: []const u8) []const u8 {
+    return std.mem.trim(u8, input, " \n\t\r");
+}
+
 fn parseGame(input: []const u8, allocator: Allocator) !Game {
     const idx = std.mem.indexOf(u8, input, ":");
-    const game_str: []const u8 = input[0..idx.?];
-    const draws_str: []const u8 = input[idx.? + 1 ..];
+    const game_str: []const u8 = trimWhitespace(input[0..idx.?]);
+    const draws_str: []const u8 = trimWhitespace(input[idx.? + 1 ..]);
 
-    std.debug.print("'{s}' || '{s}'\n", .{ game_str, draws_str });
-    return Game.init(allocator);
+    var game = Game.init(allocator);
+    var draws_it = std.mem.tokenizeScalar(u8, draws_str, ';');
+    while (draws_it.next()) |draw_str| {
+        try game.draws.append(try parseDraw(draw_str));
+    }
+
+    game.id = try parseGameId(game_str);
+    return game;
+}
+
+fn parseDraw(input: []const u8) !Draw {
+    var draw: Draw = .{
+        .red = 0,
+        .green = 0,
+        .blue = 0,
+    };
+    var it = std.mem.tokenizeScalar(u8, input, ',');
+    while (it.next()) |num_color| {
+        const nc = trimWhitespace(num_color);
+        const idx = std.mem.indexOf(u8, nc, " ");
+        const num_chars = nc[0..idx.?];
+        const color = nc[idx.? + 1 ..];
+
+        if (std.mem.eql(u8, color, "red")) {
+            draw.red = try std.fmt.parseInt(usize, num_chars, 10);
+        } else if (std.mem.eql(u8, color, "green")) {
+            draw.green = try std.fmt.parseInt(usize, num_chars, 10);
+        } else if (std.mem.eql(u8, color, "blue")) {
+            draw.blue = try std.fmt.parseInt(usize, num_chars, 10);
+        }
+    }
+    return draw;
+}
+
+fn parseGameId(input: []const u8) !usize {
+    const idx = std.mem.indexOf(u8, input, " ");
+    return std.fmt.parseInt(usize, input[idx.? + 1 ..], 10);
+}
+
+fn gameIsPossible(game: *const Game, bag: *const Draw) bool {
+    for (game.draws.items) |*draw| {
+        if (draw.red > bag.red or
+            draw.green > bag.green or
+            draw.blue > bag.blue)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 pub fn main() !void {
@@ -45,15 +96,29 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
 
+    const bag = Draw{
+        .red = 12,
+        .green = 13,
+        .blue = 14,
+    };
+
+    var sum_possible_game_ids: usize = 0;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         const game = try parseGame(line, allocator);
         defer game.deinit();
+
+        if (gameIsPossible(&game, &bag)) {
+            sum_possible_game_ids += game.id;
+        }
     }
+
+    std.debug.print("Sum of possible game id's: {d}\n", .{sum_possible_game_ids});
 }
 
 test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    const draw_str = "5 red, 6 blue, 9 green";
+    const draw = try parseDraw(draw_str);
+    try std.testing.expectEqual(draw.red, 5);
+    try std.testing.expectEqual(draw.blue, 6);
+    try std.testing.expectEqual(draw.green, 9);
 }
